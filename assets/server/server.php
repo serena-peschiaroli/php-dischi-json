6f45ddae4d503f52452895483238b2d9fa9ff15e
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-12-14 16:21:14 +0100

    Fix: json encode in server.php; add: styles in index, implementing cards

--- assets/server/server.php
+++ assets/server/server.php
@@ -5,7 +5,7 @@ $string = file_get_contents('dischi.json');
 
 // decodificare JSON in un PHP array
 $disks = json_decode($string, true);
-
+$json = json_encode($disks);
 // header per permettere al live server di inviare richieste al localhost
 
 header('Access-Control-Allow-Origin: http://127.0.0.1:5500');  

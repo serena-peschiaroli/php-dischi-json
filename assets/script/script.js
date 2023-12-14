30f9ef9622faaac869535a5c91cd1f158a948ea4
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-12-14 17:01:09 +0100

    WIP: showing disk details with function

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,15 +3,29 @@ const { createApp } = Vue;
 createApp({
     data() {
         return{
+            //inizializza selectedDisk=null
+            selectedDisk: null,
             diskList: [],
             apiUrl:'http://localhost:8888/Boolean/php-dischi-json/assets/server/server.php'
         }
         
     },
     created() {
+        //richeista axios a server
         axios.get(this.apiUrl).then((resp) => {
             this.diskList = resp.data;
             console.log(this.diskList);
         });
+    },
+    methods: {
+        //funzione x mostrare dettagli
+        displayDiskDetails(disk) {
+            this.selectedDisk = disk;
+        },
+        //funzione al click per nascondere dettagli
+        hideDetails() {
+            this.selectedDisk = null;
+        }
+
     }
 }).mount('#app');

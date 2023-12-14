93bd8e9e6c8775aa758f545c86924baaff5d79f5
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-12-14 15:57:14 +0100

    Add: headers for local request from liveserver to localhost

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,17 @@
 const { createApp } = Vue;
 
-createApp({}).mount('#app');
+createApp({
+    data() {
+        return{
+            diskList: [],
+            apiUrl:'http://localhost:8888/Boolean/php-dischi-json/assets/server/server.php'
+        }
+        
+    },
+    created() {
+        axios.get(this.apiUrl).then((resp) => {
+            this.diskList = resp.data;
+            console.log(this.diskList);
+        });
+    }
+}).mount('#app');
--- assets/server/server.php
+++ assets/server/server.php
@@ -6,8 +6,17 @@ $string = file_get_contents('dischi.json');
 // decodificare JSON in un PHP array
 $disks = json_decode($string, true);
 
+// header per permettere al live server di inviare richieste al localhost
+
+header('Access-Control-Allow-Origin: http://127.0.0.1:5500');  
+header('Access-Control-Allow-Methods: GET');  
+header('Access-Control-Allow-Headers: Content-Type');  
+
 // lista dischi
+
+
 header('Content-Type: application/json');
+
 echo json_encode($disks);
 
 ?>
